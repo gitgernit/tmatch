441f902a7d0f9dfbfad9b49bad5f2e65c8984fba
Correct Name <petr.zakharov.anton@gmail.com>
2026-02-26 01:24:17 +0300

    fix: cleanup logging

--- src/app/infra/logging/structlog.py
+++ src/app/infra/logging/structlog.py
@@ -1,54 +1,15 @@
 import logging
-import typing
 
 import structlog.processors
-from dishka import AsyncContainer
-from structlog.types import EventDict, Processor
-
-from adviser.infra.observability.sage.config.models import SageConfig
+from structlog.types import Processor
 
 
-class SageProcessor:
-    def __init__(self, sage_config: SageConfig) -> None:
-        self._config = sage_config
-
-    def __call__(self, logger: logging.Logger, method: str, event_dict: EventDict) -> EventDict:
-        event_dict["env"] = self._config.env
-        event_dict["system"] = self._config.system
-        event_dict["component"] = self._config.component
-
-        return event_dict
-
-
-class LevelProcessor:
-    LEVEL_MAP: typing.ClassVar[dict[str, str]] = {
-        "debug": "DEBUG",
-        "info": "INFO",
-        "warning": "WARN",
-        "error": "ERROR",
-        "critical": "CRITICAL",
-    }
-
-    def __call__(self, logger: logging.Logger, method: str, event_dict: EventDict) -> EventDict:
-        level = event_dict.get("level")
-
-        if isinstance(level, str):
-            event_dict["level"] = self.LEVEL_MAP.get(level.lower(), level)
-
-        return event_dict
-
-
-async def build_processors(container: AsyncContainer) -> list[Processor]:
-    sage_config = await container.get(SageConfig)
-
+async def build_processors() -> list[Processor]:
     timestamp_processor = structlog.processors.TimeStamper(fmt="iso", utc=True, key="@timestamp")
-    sage_processor = SageProcessor(sage_config)
 
     processors: list[Processor] = [
         structlog.processors.add_log_level,
         timestamp_processor,
-        sage_processor,
-        LevelProcessor(),
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
         structlog.processors.UnicodeDecoder(),
@@ -56,8 +17,8 @@ async def build_processors(container: AsyncContainer) -> list[Processor]:
     return processors
 
 
-async def configure_logging(container: AsyncContainer) -> None:
-    processors = await build_processors(container)
+async def configure_logging() -> None:
+    processors = await build_processors()
     formatter = structlog.stdlib.ProcessorFormatter(
         processor=structlog.processors.JSONRenderer(),
         foreign_pre_chain=processors,
@@ -80,7 +41,5 @@ async def configure_logging(container: AsyncContainer) -> None:
     structlog.configure(
         processors=[*processors, structlog.processors.JSONRenderer()],
         context_class=dict,
-        logger_factory=structlog.stdlib.LoggerFactory(),
-        wrapper_class=structlog.stdlib.BoundLogger,
         cache_logger_on_first_use=True,
     )
